ccf1b4f9c89603bdb5e4679fabd9c5340fcd22b5
chtmorris <agoosester@gmail.com>
2015-11-14 16:05:08 +0800

    Added new Helpers.swift file and included Delay function

---
+++ heli-Trump/Helpers.swift
@@ -0,0 +1,22 @@
+//
+//  Helpers.swift
+//  heli-Trump
+//
+//  Created by Charlie Morris on 14/11/2015.
+//  Copyright © 2015 chtmorris. All rights reserved.
+//
+
+import Foundation
+
+class Helper{
+    
+    static func delay(delay:Double, closure:()->()) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), closure)
+    }
+    
+}
